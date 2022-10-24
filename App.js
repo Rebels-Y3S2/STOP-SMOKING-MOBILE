2a5807d5ceb48fffa15b067aa07c985c5b6798f3
Shehan Bartholomeusz <shehan.bartholomeusz@pearson.com>
2022-10-24 19:15:20 +0530

    Styles

--- App.js
+++ App.js
@@ -6,29 +6,52 @@
  * @flow strict-local
  */
 
- import React from 'react';
- import { NavigationContainer } from '@react-navigation/native';
- import { createNativeStackNavigator } from '@react-navigation/native-stack';
- import Test from './src/pages/test';
- import Home from './src/pages/home';
- 
- const Stack = createNativeStackNavigator();
- 
- const App = ()  => {
- 
-   return (
-     <NavigationContainer>
-     <Stack.Navigator>
-       <Stack.Screen
-         name="Test"
-         component={Test}
-         // options={{ title: 'Welcome' }}
-       />
-       <Stack.Screen name="Home" component={Home} />
-     </Stack.Navigator>
-   </NavigationContainer>
-   );
- };
- 
- export default App;
- 
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Test from './src/pages/test';
+import Home from './src/pages/home';
+import { Button, StyleSheet } from 'react-native';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Test"
+          component={Test}
+          options={getNavigatorStyles('Welcome')}
+        />
+        <Stack.Screen 
+          name="Home" 
+          component={Home} 
+          options={getNavigatorStyles('Home')} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+function getNavigatorStyles(componentName) {
+  return {
+    title: componentName,
+    headerStyle: {
+      backgroundColor: '#1658CD',
+      borderColor: '#1658CD'
+    },
+    headerTitleStyle: {
+      font: 'Roboto',
+      fontStyle: 'normal',
+      fontWeight: '500',
+      fontSize: '20px',
+      lineHeight: '24px',
+      letterSpacing: '0.15px',
+      color: '#FFFFFF'
+    }
+  };
+}
+
+
+export default App;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import PopupContainer from '../components/Contaner/PopupContainer';
+import BigHeaderBackground from '../components/HeaderBackground/HeaderBackground';
 
 const styles = StyleSheet.create({
     sectionTitle: {
-      fontSize: 40,
-      fontWeight: '600',
-      textAlign: 'center',
-      marginTop: '50%'
+        fontSize: 40,
+        fontWeight: '600',
+        textAlign: 'center',
+        marginTop: '50%'
     }
 });
 
 export default function Home({ navigation, route }) {
-    return(
+    return (
         <View>
-            <Text style={[styles.sectionTitle]}>This is home and this screen navigated from {route.params.name}</Text>
+            <BigHeaderBackground />
+            <PopupContainer firstContainer>
+                <Text style={[styles.sectionTitle]}>This is home and this screen navigated from {route.params.name}</Text>
+            </PopupContainer>
+            <PopupContainer>
+                <Text style={[styles.sectionTitle]}>This is home and this screen navigated from {route.params.name}</Text>
+            </PopupContainer>
         </View>
     );
 }
