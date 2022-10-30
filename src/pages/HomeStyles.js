3918a91caf8a8bf1f21874b29772d432412d027c
ShivaaniDushya <it20006884@my.sliit.lk>
2022-10-30 15:43:36 +0530

    Home user interface done

---
+++ src/pages/HomeStyles.js
@@ -0,0 +1,66 @@
+import { StyleSheet } from "react-native"
+
+export const homestyles = StyleSheet.create({
+    welcomephrase:{
+        fontSize: 25,
+        marginTop: 38,
+        marginLeft: 31,
+        font: 'Roboto',
+    },
+    imageinstruction: {
+        fontSize: 18,
+        marginTop: 38,
+        textAlign: 'center',
+        font: 'Roboto',
+    },
+    imagecontainer: {
+        width: 200, 
+        height: 120,
+        marginLeft: 60,
+        marginRight: 60,
+        marginTop: 20,
+    },
+    challengename: {
+        fontSize: 24,
+        color: "#314371",
+        font: 'Roboto',
+        textAlign: 'center',
+        marginTop: 10,
+        marginBottom: 38
+    },
+    popupcontainerMargin: {
+        marginBottom: 20
+    },
+    popupcontainerMarginSecond: {
+        marginBottom: 30
+    },
+    stopsmokeImageContainer: {
+        width: 300, 
+        height: 200,
+        marginLeft: 12,
+        marginRight: 12,
+        marginTop: 20,
+        font: 'Roboto',
+    },
+    instructionTitle: {
+        fontSize: 18,
+        marginLeft: 18,
+        marginTop: 20,
+        marginBottom: 20,
+        font: 'Roboto',
+    },
+    pointsContainer: {
+        fontSize: 15,
+        marginLeft: 30,
+        marginRight: 30,
+        marginBottom: 10,
+        font: 'Roboto',
+    },
+    pointsContainerLast: {
+        fontSize: 15,
+        marginLeft: 30,
+        marginRight: 30,
+        marginBottom: 30,
+        font: 'Roboto',
+    }
+})
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,38 @@
 import React from "react";
-import { View, Button, Text, StyleSheet } from "react-native";
+import { View, Button, Text, StyleSheet, ScrollView , TextInput, ActivityIndicator} from "react-native";
+import BigHeaderBackground from "../components/HeaderBackground/HeaderBackground.js";
+import PopupContainer from "../components/Contaner/PopupContainer.js";
+import { homestyles } from "./HomeStyles.js";
+import { Image } from 'react-native-elements';
 
 const Home = () => {
   return (
-    <View style={styles.center}>
-      <Text>This is the Home screen</Text>
+    <View>
+      <ScrollView>
+        <BigHeaderBackground/>
+        <PopupContainer firstContainer style={homestyles.popupcontainerMargin}>
+          <Text variant='subtitle 2' style={homestyles.welcomephrase}>Hey John !</Text>
+          <Text variant='subtitle 2' style={homestyles.imageinstruction}>Click the image to navigate to your challenge</Text>
+          <Image
+            source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSu_lhDKyIPN98vfAYrx1vLeVoIbjXt6N2siWtu4cx3hm0n-YBrvzl25lngvIILvdSElz4&usqp=CAU" }}
+            style={homestyles.imagecontainer}
+          />
+          <Text variant='subtitle 2' style={homestyles.challengename}>No more cigarettes</Text>
+        </PopupContainer>
+
+        <PopupContainer firstContainerstyle={homestyles.popupcontainerMarginSecond}>
+          <Image
+            source={{ uri: "https://www.lanermc.org/hubfs/stop-smoking-now.993x0-is.jpg" }}
+            style={homestyles.stopsmokeImageContainer}
+          />
+          <Text variant='subtitle 2' style={homestyles.instructionTitle}>Let's stop smoking...</Text>
+          <Text variant='subtitle 2' style={homestyles.pointsContainer}>Smoking causing nearly 8 million deaths in 2022.</Text>
+          <Text variant='subtitle 2' style={homestyles.pointsContainer}>90% of new smokers become addicted by age 25</Text>
+          <Text variant='subtitle 2' style={homestyles.pointsContainerLast}>Smoking is a major risk factor that threatens the health of people worldwide</Text>
+        </PopupContainer>
+      </ScrollView>
     </View>
   );
 };
-const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
-  },
-});
+
 export default Home;
