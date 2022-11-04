233fbbd95e741c4cbfab752b7877c7ee15de4f2a
Shehanx86 <it20274702@my.sliit.lk>
2022-11-04 15:28:58 +0530

    challenge features

--- src/pages/Challenge Management/challengeView/ChallengeView.js
+++ src/pages/Challenge Management/challengeView/ChallengeView.js
@@ -1,33 +1,45 @@
 import React from "react";
-import { View, StyleSheet, SafeAreaView, ScrollView } from "react-native";
+import { View, SafeAreaView, ScrollView } from "react-native";
 import PopupContainer from "../../../components/Contaner/PopupContainer";
 import ChallengeProgress from "../../../components/Challenges/ChallengeProgress/ChallengeProgress";
 import BigHeaderBackground from "../../../components/HeaderBackground/HeaderBackground";
 import Todo from "../../../components/Challenges/Todo/Todo";
+import { ChallengeConstants } from "../../../util/Constants/ChallengeConstants";
+import styles from "./styles";
 
-const todo = ["Dont smoke", "Drink water", "eat well"];
+export default function ChallengeView({ route }) {
+  const { daysLeft, name, completed, tasks, onGoingDate } = route.params;
 
-export default function ChallengeView() {
+  // Will change these with API keep this here for now - approve karapn @dulshan
+  function getAllowedCigs(cigCount) {
+    if (cigCount == 0) {
+      return "You are not allowed to smoke today";
+    } else if (cigCount == 1) {
+      return `You are allowed to smoke only ${cigCount} cigarrate today`;
+    } else {
+      return `You are allowed to smoke ${cigCount} cigarrates today`;
+    }
+  }
+
+  const todo = [...tasks.tasks, ...[getAllowedCigs(tasks.noOfCigs)]];
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.scrollView}>
         <BigHeaderBackground />
         <PopupContainer firstContainer>
           <View>
-            <ChallengeProgress title={"No more cigs"} progress={54} />
+            <ChallengeProgress
+              name={name}
+              completed={completed}
+              daysLeft={daysLeft}
+            />
           </View>
         </PopupContainer>
         <PopupContainer>
-          <Todo title={"Today's challenge"} todoList={todo} />
+          <Todo title={`${ChallengeConstants.DAY} ${onGoingDate}`} todoList={todo} />
         </PopupContainer>
       </ScrollView>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  scrollView: {},
-});
