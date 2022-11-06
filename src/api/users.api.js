74274ce72314aef6c5a8e455d7f6b2025d329731
ShivaaniDushya <it20006884@my.sliit.lk>
2022-11-06 11:50:19 +0530

    Update user API integration done

--- src/api/users.api.js
+++ src/api/users.api.js
@@ -6,7 +6,11 @@ const addUser = (data) => apiInstance.post(`/users/register`, data);
 // User Login
 const loginUser = (data) => apiInstance.post(`/users/login`, data);
 
+//  Update user details
+const updateUser = (id, data) => apiInstance.put(`/users/${id}`, data);
+
 export const userRequests = {
     addUser,
     loginUser,
+    updateUser,
 }
